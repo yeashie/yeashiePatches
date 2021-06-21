3b8014a576f7f2ee380a22470753843115b90175
yeashie <kristafer06@gmail.com>
2021-06-21 16:02:19 -0400

    GBW

---
+++ GreatBedWar/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "GreatBedWar",
+	"servers": [
+		"https://raw.githubusercontent.com/yeashie/yeashiePatches/master/GreatBedWar/"
+	],
+	"title": "Great Bed Wars"
+}
---
+++ repo.js
@@ -0,0 +1,11 @@
+{
+	"contact": "yeashie#0001",
+	"id": "yeashie",
+	"patches": {
+		"GreatBedWar": "Great Bed Wars"
+	},
+	"servers": [
+		"https://raw.githubusercontent.com/yeashie/yeashiePatches/master/"
+	],
+	"title": "yeashie"
+}
