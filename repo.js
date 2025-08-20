d6c42e6de36650d62516ce5be8f096ea1a5d2284
yeashie <kristafer06@gmail.com>
2025-08-20 07:04:14 -0400

    Update repo.js

--- repo.js
+++ repo.js
@@ -2,7 +2,8 @@
 	"contact": "yeashie#0001",
 	"id": "yeashie",
 	"patches": {
-		"GreatBedWar": "Great Bed Wars"
+		"GreatBedWar": "Great Bed Wars",
+		"TH20AirhornBreak": "Replaces TH20 FW Hyper-Breaks with the MLG Airhorn SFX to make Breaks more noticeable"
 	},
 	"servers": [
 		"https://raw.githubusercontent.com/yeashie/yeashiePatches/master/"
