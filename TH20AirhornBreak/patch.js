3f8b33302e342b148fc432215723ee570a0d4714
yeashie <kristafer06@gmail.com>
2025-08-20 08:11:57 -0400

    Add files via upload

---
+++ TH20AirhornBreak/patch.js
@@ -0,0 +1,10 @@
+{
+	"dependencies": [
+		"nmlgc/base_tsa"
+	],
+	"id": "TH20AirhornBreak",
+	"servers": [
+		"https://raw.githubusercontent.com/yeashie/yeashiePatches/master/TH20AirhornBreak/"
+	],
+	"title": "Replaces TH20 FW Hyper-Breaks with the MLG Airhorn SFX to make Breaks more noticeable"
+}
